a94e3329e4346382c7cee8bd7a4abf1460962e33
Phoebe Orwell <phoebe.orwell@gmail.com>
2022-11-14 16:08:04 +0100

    Iteration7 complete

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,6 +1,7 @@
 import ProfileImage from "./ProfileImage"
 import User from "./User"
 import Timestamp from "./Timestamp"
+import Message from "./Message"
 
 function Tweet({tweet}) {
   return (
@@ -10,12 +11,9 @@ function Tweet({tweet}) {
       <div className="body">
         <div className="top">
         <User user={tweet.user}/>
-          <Timestamp timestamp={tweet.timestamp}/>
+        <Timestamp timestamp={tweet.timestamp}/>
         </div>
-
-        <p className="message">
-         {tweet.message}
-        </p>
+        <Message message={tweet.message}/>
 
         <div className="actions">
           {/* Font Awesome icons */}
